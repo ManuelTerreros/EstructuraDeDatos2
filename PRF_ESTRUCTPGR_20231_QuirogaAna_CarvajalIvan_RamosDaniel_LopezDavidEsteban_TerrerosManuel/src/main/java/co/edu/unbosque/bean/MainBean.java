0655b88ef0bbef987b816de0ea93081795111340
carol <carol@LAPTOP-9D94RA61>
2023-05-25 23:02:14 -0500

    Puse la funcion para llevar los nombres de los jugadores a la otra pagina

--- PRF_ESTRUCTPGR_20231_QuirogaAna_CarvajalIvan_RamosDaniel_LopezDavidEsteban_TerrerosManuel/src/main/java/co/edu/unbosque/bean/MainBean.java
+++ PRF_ESTRUCTPGR_20231_QuirogaAna_CarvajalIvan_RamosDaniel_LopezDavidEsteban_TerrerosManuel/src/main/java/co/edu/unbosque/bean/MainBean.java
@@ -1,8 +1,57 @@
 package co.edu.unbosque.bean;
 
+import java.io.Serializable;
+
 import javax.faces.bean.ManagedBean;
 
+import co.edu.unbosque.model.LogicaPrincipal;
+
 @ManagedBean
-public class MainBean {
+public class MainBean implements Serializable{
+	
+	private static final long serialVersionUID = -2152389656664659476L;
+	private String usuario1="", usuario2="", aprendiz="";
+	private LogicaPrincipal log;
+	
+	public String navegar() {
+
+		return "juego.xhtml";
+	}
+	
+
+
+	public String getUsuario1() {
+		return usuario1;
+	}
+
+	public void setUsuario1(String usuario1) {
+		this.usuario1 = usuario1;
+	}
+
+	public String getUsuario2() {
+		return usuario2;
+	}
+
+	public void setUsuario2(String usuario2) {
+		this.usuario2 = usuario2;
+	}
+
+	public String getAprendiz() {
+		return aprendiz;
+	}
+
+	public void setAprendiz(String aprendiz) {
+		this.aprendiz = aprendiz;
+	}
+
+	public LogicaPrincipal getLog() {
+		return log;
+	}
+
+	public void setLog(LogicaPrincipal log) {
+		this.log = log;
+	}
+	
+	
 
 }
